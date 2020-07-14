7a36183a12ded9be7aeff948e2e9689c780c4972
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-14 12:51:01 +0000

    Deploy Solirius/hackathon-spelling-app to github.com/Solirius/hackathon-spelling-app.git:gh-pages

---
+++ precache-manifest.74e4bf79d4dbde55b9cd732381db52c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166e4b1d801372b3629da49c4250c998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ce340514d9227c7e296",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "580b022c5f6ef0b8a68d",
+    "url": "/static/js/2.fc2b6d71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc2b6d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce340514d9227c7e296",
+    "url": "/static/js/main.e6240ae9.chunk.js"
+  },
+  {
+    "revision": "16fc0d283374180366b4",
+    "url": "/static/js/runtime-main.f4772e17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
